87e98a864550ac007cca3773872dea08c924eaf8
Karina Smoje <karinasmoje@gmail.com>
2018-07-26 02:01:53 -0400

    first commit

---
+++ assets/src/js/app.js
@@ -0,0 +1,46 @@
+/*let options = {
+    enableHighAccuracy: true,
+    timeout: 6000,
+    maximumAge: 0
+};
+
+navigator.geolocation.getCurrentPosition( succes, error, options );
+
+
+function succes(position){
+let coordenadas = position.coords;
+
+console.log("Tu posición actual es: ");
+console.log("Latitud : " + coordenadas.latitude);
+console.log("Longitud" + coordenadas.longitude);
+console.log("Más o menos " + coordenadas.accuracy + " metros.");
+};
+
+function error(error) {
+    console.warn("ERROR (" + error.code + "): " + error.message);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
